7aa2402216331b6d0ceab6237260649eb0562301
moataz hjaiji <moatazhjaiji@takiacademyteam.com>
2023-11-29 10:04:32 +0100

    create slider front

--- assets/shop/entry.js
+++ assets/shop/entry.js
@@ -1,1 +1,2 @@
 // In this file you can import assets like images or stylesheets
+import './slider/index';
---
+++ assets/shop/slider/index.js
@@ -0,0 +1,16 @@
+import './swiper.css';
+const swiper = new Swiper('.swiper', {
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  scrollbar: {
+    el: '.swiper-scrollbar',
+  },
+});
+
